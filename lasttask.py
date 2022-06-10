b2e5be4baa8853af9a6d7584936cec7a706449b7
Азат <seitan71azat@gmail.com>
2022-06-10 16:30:51 +0300

    last commt

---
+++ lasttask.py
@@ -0,0 +1,16 @@
+n = int(input())
+k = int(input())
+
+
+def F(n, k):
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+
+    return F(n - 1, k) + F(n - 2, k) * k
+
+
+print(F(n, k))
